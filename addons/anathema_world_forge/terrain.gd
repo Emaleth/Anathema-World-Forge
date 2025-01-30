d070372c65d78c0300749fb7e77165e679d0165e
Emaleth <48322449+Emaleth@users.noreply.github.com>
2025-01-30 19:35:01 +0100

    update

--- addons/anathema_world_forge/terrain.gd
+++ addons/anathema_world_forge/terrain.gd
@@ -1,18 +1,23 @@
 @tool
 extends StaticBody3D
 
+@export_category("basic")
 @export var generate := false
 @export var heightmap : Texture2D
 @export var normalmap : Texture2D
 @export var clipmap_mesh : Mesh
-@export var heightmap_max_height : int
-@export var heightmap_min_height : int
+@export var max_height : int
+
+@export_category("slope textures")
+@export var flat : Texture2D
+@export var slope : Texture2D
 
 @onready var terrain_shader : Shader = preload("res://addons/anathema_world_forge/terrain.gdshader")
 
 var terrain_material : ShaderMaterial
 var mesh_instance : MeshInstance3D
 var collision_shape : CollisionShape3D
+
 
 func _ready() -> void:
 	_generate()
@@ -21,9 +26,9 @@ func _process(delta: float) -> void:
 	if generate == false: return
 	_generate()
 	generate = false
-	
+
 func move_terrain(pos : Vector3) -> void:
-	global_transform.origin = Vector3(pos.x, 0, pos.z).snapped(Vector3(1, 1, 1))
+	mesh_instance.global_transform.origin = Vector3(pos.x, 0, pos.z).snapped(Vector3(1, 1, 1))
 
 func _generate() -> void:
 	_clear_children()
@@ -36,14 +41,14 @@ func _clear_children():
 		i.queue_free()
 		
 func _create_terrain_collision_shape():
-	#FIXME: COLLISION SHAPE DOESN'T SHOW IN EDITOR
 	collision_shape = CollisionShape3D.new()
 	self.add_child(collision_shape)
+	collision_shape.owner = self
 	var hms = HeightMapShape3D.new()
 	var hm_img = heightmap.get_image()
 	hm_img.decompress()
 	hm_img.convert(Image.FORMAT_RH)
-	hms.update_map_data_from_image(hm_img, heightmap_min_height, heightmap_max_height)
+	hms.update_map_data_from_image(hm_img, 0.0, max_height)
 	collision_shape.shape = hms
 
 func _configure_terrain_material():
@@ -51,12 +56,15 @@ func _configure_terrain_material():
 	terrain_material.shader = terrain_shader
 	terrain_material.set("shader_parameter/heightmap", heightmap)
 	terrain_material.set("shader_parameter/normalmap", normalmap)
-	terrain_material.set("shader_parameter/height", heightmap_max_height)
+	terrain_material.set("shader_parameter/height", max_height)
 	terrain_material.set("shader_parameter/heightmap_scale", Vector2.ONE)
+	terrain_material.set("shader_parameter/flat_texture", flat)
+	terrain_material.set("shader_parameter/slope_texture", slope)
 
 func _create_terrain_mesh_instance():
 	mesh_instance = MeshInstance3D.new()
 	self.add_child(mesh_instance)
+	mesh_instance.owner = self
 	mesh_instance.mesh = clipmap_mesh
 	mesh_instance.material_override = terrain_material
 	
